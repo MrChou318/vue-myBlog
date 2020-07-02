96a00aa5e572a17d3e1567ac5ecc6445c3aaaf02
unknown <603780785@qq.com>
2020-07-02 22:58:02 +0800

    头部菜单栏 按钮渐变

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,76 @@
 <template>
   <div id="app">
+    <div class='menu' :class="{'menu-top': scrollHeight === 0, 'menu-down': scrollMode === 'down', 'menu-up': scrollMode === 'up'}">
+      <span style="font-size: 40px;" class="">12d123231233</span>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      scrollHeight: 0,
+      scrollMode: ''
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.scrollHandle)
+  },
+  methods: {
+    scrollHandle (e) {
+      // 获取页面滚动高度
+      this.scrollHeight = e.srcElement.scrollingElement.scrollTop
+    }
+  },
+  watch: {
+    scrollHeight (newVal, oldVal) {
+      if (newVal > oldVal) {
+        // 往下滚动...
+        this.scrollMode = 'down'
+      } else {
+        // 往上滚动...
+        this.scrollMode = 'up'
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 #app {
+  /*自带的*/
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  /*顶部目录*/
+  .menu {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 50px;
+    background-color: #e1e3e8;
+    z-index: 999;
+    opacity: 0.7;
+  }
+  .menu-down {
+    display: none;
+  }
+  .menu-up {
+    opacity: 0.7;
+    animation: animate-menu-up 2s linear;
+  }
+  @keyframes animate-menu-up {
+    0% {height: 0;}
+    100% {height: 50px;}
+  }
+  .menu-top {
+    transition: 0.6s;
+    background-color: rgba(0, 0, 0, 0);
+  }
 }
 </style>
