f163f66b0ab5a02743c838823f85f6973a3bfec5
unknown <603780785@qq.com>
2020-07-14 07:07:28 +0800

    项目初始化并修改图片等转base64大小限制

--- src/App.vue
+++ src/App.vue
@@ -27,32 +27,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  /*顶部目录*/
-  .menu {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 50px;
-    background-color: #e1e3e8;
-    z-index: 999;
-    opacity: 0.7;
-  }
-  .menu-down {
-    display: none;
-  }
-  .menu-up {
-    opacity: 0.7;
-    animation: animate-menu-up 1s linear;
-  }
-  @keyframes animate-menu-up {
-    0% {transform: translateY(-50px);}
-    50% {transform: translateY(-25px);}
-    100% {transform: translateY(0px);}
-  }
-  .menu-top {
-    transition: 0.6s;
-    background-color: rgba(0, 0, 0, 0);
-  }
 }
 </style>
