8e19e1562458c03609e987626f654d8ee2c574c0
MrChou318 <41139656+MrChou318@users.noreply.github.com>
2020-07-20 18:02:41 +0800

    Create BlogInfo.vue

---
+++ src/page/components/BlogInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <BlogBriefCard></BlogBriefCard>
+  </div>
+</template>
+
+<script>
+import BlogBriefCard from './BlogBriefCard'
+export default {
+  components: {
+    BlogBriefCard
+  }
+}
+
+</script>
+<style lang='less' scoped>
+
+</style>
