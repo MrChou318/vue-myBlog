0b8c4410afc75ea8f56603af82b7b13f04b6db7e
MrChou318 <41139656+MrChou318@users.noreply.github.com>
2020-09-30 17:39:07 +0800

    Update Home.vue

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -19,53 +19,42 @@
 <script>
 import Hello from './components/Hello'
 import ArticleBrief from './components/ArticleBrief'
-import BlogInfo from '../components/BlogInfo'
+import BlogInfo from '@/page/components/BlogInfo'
 export default {
   name: 'Home',
   components: {
     Hello,
     ArticleBrief,
     BlogInfo
+  },
+  mounted () {
+  },
+  methods: {
   }
 }
 </script>
 
 <style lang="less" scoped>
   .hello {
-    animation: animate-wrapper-merge .8s linear;
-    @keyframes animate-wrapper-merge {
-      // 之所以是1%不是0%是因为防止每刷新页面一次，页面就往下滚动一点；添加0%是防止双闪
-      0% {transform: translateY(0);opacity: 0;}
-      1% {transform: translateY(-8%);opacity: 0;}
-      50% {transform: translateY(-3.5%);opacity: 0.5;}
-      100% {transform: translateY(0);opacity: 1;}
-    }
+    animation: animate-wrapper-merge-down .8s ease-out;
   }
   .content-wrapper {
     width: 100%;
     box-sizing: border-box;
     display: flex;
     justify-content: center;
-    animation: animate-content-merge .8s linear;
-    @keyframes animate-content-merge {
-      0% {transform: translateY(0);opacity: 0;}
-      1% {transform: translateY(8%);opacity: 0;}
-      50% {transform: translateY(3.5%);opacity: 0.5;}
-      100% {transform: translateY(0);opacity: 1;}
-    }
+    animation: animate-content-merge-up .8s ease-out;
     .content {
       display: flex;
       justify-content: space-between;
       width: 100vw;
       max-width: 22rem;
-      border: 1px solid red;
       padding: .5rem .2rem;
       .article-list-wrapper {
         width: 72%;
       }
       .blog-info-wrapper {
         width: 25%;
-        border: 1px solid black;
       }
       @media screen and (max-width: 900px) {
         .article-list-wrapper {
