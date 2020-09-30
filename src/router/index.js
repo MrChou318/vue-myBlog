d76633cc9547d13e5ce9ed9b25d96ac0ee3187a9
MrChou318 <41139656+MrChou318@users.noreply.github.com>
2020-09-30 17:40:40 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 const Home = () => import('@/page/home/Home')
+const Archive = () => import('@/page/archive/Archive')
+const Details = () => import('@/page/details/Details')
+const MD = () => import('@/page/md/MD')
+// test
+const Alphabet = () => import('@/page/test/Alphabet')
+const Btn = () => import('@/page/test/Btn')
 
 Vue.use(Router)
 
@@ -10,6 +16,32 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/archive',
+      name: 'Archive',
+      component: Archive
+    },
+    {
+      path: '/details',
+      name: 'Details',
+      component: Details
+    },
+    {
+      path: '/md',
+      name: 'MD',
+      component: MD
+    },
+    // test
+    {
+      path: '/alphabet',
+      name: 'Alphabet',
+      component: Alphabet
+    },
+    {
+      path: '/btn',
+      name: 'Btn',
+      component: Btn
     }
   ]
 })
