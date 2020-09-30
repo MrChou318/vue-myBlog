9db965daf494ce3721cdec8fcbbb7eb1dd2c46aa
MrChou318 <41139656+MrChou318@users.noreply.github.com>
2020-09-30 17:41:25 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,14 +5,25 @@ import App from './App'
 import router from './router'
 import less from 'less'
 import 'lib-flexible/flexible.js'
+import utils from './js/utils'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import store from '@/store/index.js'
+import { Select, Option, Input } from 'element-ui'
 
 Vue.use(less)
+Vue.use(mavonEditor)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Input)
 Vue.config.productionTip = false
+Vue.prototype.utils = utils
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
